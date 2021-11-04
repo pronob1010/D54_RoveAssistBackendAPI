22f3e25a50f5bb85f6862e350311604459b7bc1d
Pronob Mozumder <pronobmozumder.info@gmail.com>
2021-11-05 03:02:43 +0600

    company info added

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from django.conf.urls.static import static
+from django.conf import settings
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
     path('note/', include('note.urls')),
     path('agent/', include('travel_agent.urls')),
 ]
+
+urlpatterns+=static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- backend/travel_agent/models.py
+++ backend/travel_agent/models.py
@@ -21,3 +21,19 @@ def create_agent(sender, instance, created, **kwargs):
     if agent.objects.filter(agent= instance).exists() == False:
         if created == False and instance.is_agent == True:
             agent.objects.create(agent = instance)
+
+
+class CompanyInfo(models.Model):
+    company_owner = models.OneToOneField(agent, on_delete=CASCADE, related_name="company_root")
+    primary_logo = models.ImageField(upload_to='company/logo')
+    primary_background = models.ImageField(upload_to='company/background', null=True, blank=True)
+    short_description = models.TextField(max_length=200, null=True)
+
+    def __str__(self):
+        return self.company_owner.company_name+"'s info"
+
+@receiver(post_save, sender=agent)
+def create_company_profile(sender, instance, created, **kwargs):
+    if CompanyInfo.objects.filter(company_owner = instance).exists() == False:
+        if created == True:
+            CompanyInfo.objects.create(company_owner = instance)
