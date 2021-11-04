8146142917f6aca5b2eba1ac5d62454954417eb0
Pronob Mozumder <pronobmozumder.info@gmail.com>
2021-11-04 22:33:35 +0600

    travel agent 0.1

--- backend/travel_agent/models.py
+++ backend/travel_agent/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.db.models.deletion import CASCADE
+from accounts.models import *
+class tagent(models.Model):
+    agent = models.ForeignKey(User, on_delete=CASCADE)
+    on_review = models.BooleanField(default=False)
+    on_hold = models.BooleanField(default=False)
+    suspended = models.BooleanField(default=False)
 
-# Create your models here.
+    updated_on = models.DateTimeField(auto_now=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.agent.email
