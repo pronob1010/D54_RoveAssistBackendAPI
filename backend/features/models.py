02dcd65ea26d055a1fd1eb4648b715e7979f930d
Tanvir <ibnmizan7878@gmail.com>
2021-11-18 05:07:03 +0600

    web deploy test

--- backend/features/models.py
+++ backend/features/models.py
@@ -3,9 +3,9 @@ from django.db.models.deletion import CASCADE
 
 # Create your models here.
 class postExtra(models.Model):
-    title = models.CharField(max_length=30)
+    title = models.CharField(max_length=300)
     cover_image= models.ImageField(upload_to='company/post_image', null=True, blank=True)
-    description = models.TextField(max_length=10000, null= True, blank=True)
+    description = models.TextField(max_length=20000, null= True, blank=True)
 
     latitude = models.CharField(max_length=50, null= True, blank=True)
     longitude = models.CharField(max_length=50, null= True, blank=True)
